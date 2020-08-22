fb228c78822c659e18969d5df3146859b2db00e2
Daniel Renteria <d.renteria_a@yahoo.com>
2020-08-22 16:05:25 -0400

    Fix layout margins for comment and header

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-//make header and change the font type to correct one
-//make an input box for name and comment content
-//make a button for adding the comment
-//start a table or list to input comments
+//change header font type to correct one
+//fix the header so the line starts right below the text
+
+//start <p> to input comments
+    //figure out how to center comment blocks on the page
+    //figure out the template you need for a single comment
+    // use this template to identify what you need to insert with JS
+    //setup JS to programmatically add and insert comments
+        //Access the comment <p> element with JQuery
+        //setup a way to change it to the input from the form
+        //save the name from the form to a variable
+        //figure out how to add a variable to the end of an inner html
+            //You know how to do this!!!! REMEMBER!!!
+            //JQuery really helps with this!!
+        //setup a click event listener on the post button so it creates the required elements and inserts them to the page
+        //Make sure all the comments are separated correctly by a line like the header
