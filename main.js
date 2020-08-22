a6cdcc52205f35ed769d7175d9f7670c4395ddc9
Daniel Renteria <d.renteria_a@yahoo.com>
2020-08-22 17:20:29 -0400

    Fix styling

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 //change header font type to correct one
-//fix the header so the line starts right below the text
 
 //start <p> to input comments
-    //figure out how to center comment blocks on the page
     //figure out the template you need for a single comment
     // use this template to identify what you need to insert with JS
     //setup JS to programmatically add and insert comments
-        //Access the comment <p> element with JQuery
+        //Access the comment class with JQuery
         //setup a way to change it to the input from the form
         //save the name from the form to a variable
         //figure out how to add a variable to the end of an inner html
