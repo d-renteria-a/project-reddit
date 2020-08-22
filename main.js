510a82c8bc5a25e4b0163145f64902a704135150
Daniel Renteria <d.renteria_a@yahoo.com>
2020-08-22 13:49:39 -0400

    Initial commit and add header

---
+++ main.js
@@ -0,0 +1,4 @@
+//make header and change the font type to correct one
+//make an input box for name and comment content
+//make a button for adding the comment
+//start a table or list to input comments
