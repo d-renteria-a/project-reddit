5515a83bfb173827a95db636c9ec35763bfe3d48
Daniel Renteria <d.renteria_a@yahoo.com>
2020-08-24 00:43:59 -0400

    Add Final functionality

--- main.js
+++ main.js
@@ -1,40 +1,93 @@
 //change header font type to correct one
-
-//start <p> to input comments
-    //*figure out the template you need for a single comment*
-        // use this template to identify what you need to insert with JS
-    
-    //setup JS to programmatically add and insert comments
-        //Take info from form
-        //Insert it into comment template
-        //push into the html   
-    
-    
-    
-    
-        //Access the comment class with JQuery
-        //setup a way to change it to the input from the form
-        //save the name from the form to a variable
-        //figure out how to add a variable to the end of an inner html
-            //You know how to do this!!!! REMEMBER!!!
-            //JQuery really helps with this!!
-        //setup a click event listener on the post button so it creates the required elements and inserts them to the page
-        //Make sure all the comments are separated correctly by a line like the header
+//Text needs to wrap inside parent element
+//If theres more time make sure the styles look exactly like they do in the pictures
+//check the comment toggle for double clicking
 
 
-//
 
-$('button').click(function () {
+
+$('.post-button').click(function () {
     let $newPost = $('#new-post-text')[0].value;
     let $newName = $('#new-post-name')[0].value;
+
+    let styleClasses = 'class=' + '"col-md-6 col-md-offset-3 page-header post-block"';
+    let postClass = 'class=' + '"post-text"'
+    let postedByClass = 'class=' + '"post-by"';
+    let nameClass = 'class=' + '"post-name"';
+    let removePost = '<span class="remove-post">remove</span>';
+    let commentPost = `<span class="comment-toggle">comments</span>`
+    let postCount = 1;
     
-    let styleClasses = 'class=' +'"col-md-6 col-md-offset-3 page-header comment-block"';
-    let postByClass = 'class=' + '"post-by"';
-    let nameClass = 'class=' +'"comment-name"';
-    
-    $('.comments').append(`<div ${styleClasses}><p>${$newPost}</p>
-    <p ${postByClass}>Posted By: <h5 ${nameClass}>${$newName}</h5></p></div>`)
+
+    if($newPost == 0) {
+        alert('Your post is empty!');
+        return false
+    }
+
+    const createNewPost = function() {
+        $('.posts').append(`<div ${styleClasses}>${removePost} ${commentPost}<p ${postClass}>${$newPost}</p><br>
+        <div class="comment-block"></div>
+        <p ${postedByClass}>Posted By: <h5 ${nameClass}>${$newName}</h5></p></div>`);
+        postCount++
+    }
+
+    createNewPost();
     
     $('#new-post-text')[0].value = '';
     $('#new-post-name')[0].value = '';
+
+    $('.remove-post').click(function () {
+        $(this).parent().remove();
+    })
+
+    $('.comment-toggle').click(function () {
+        let selectedComment = $(this).parent().find('.comment-block')
+        let selectedCommentClass = selectedComment.attr('class')
+        
+        if(selectedComment.children().length < 1) {
+            addCommentFields(selectedComment);
+        }
+
+        if(selectedCommentClass === 'comment-block hide') {
+            selectedComment.attr('class', 'comment-block')
+        } else {
+            selectedCommentClass += ' hide';
+            selectedComment.attr('class', selectedCommentClass)
+        }
+    })
 })
+
+
+const addCommentFields = function(comment) {
+    comment.append(
+    `<form>
+        <div class="form-group">
+            <input type="text" class="form-control comment-input" 
+                id="new-comment-text" 
+                placeholder="Comment Text">
+            <input type="text" class="form-control comment-input" 
+                id="new-comment-name" 
+                placeholder="User Name">                                                    
+            <button type="button" class="btn btn-default comment-button">Post Comment</button>
+        </div>
+    </form>`)
+   
+    $('.comment-button').click(function () {
+        let newComment = $('#new-comment-text')[0].value;
+        let userName = $('#new-comment-name')[0].value;
+        let commentStyleClasses = 'class=' + '"comments"';
+        let commentSelector = $(this).parents('.comment-block')
+        let timesGlyph = '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>'
+
+        const insertTimesGlyph = function() {
+            let timesGlyph = '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>';
+            $('.comment-name').append(timesGlyph)
+        }
+
+        commentSelector.prepend(`<div ${commentStyleClasses}><span>${newComment}Posted By: ${userName}</span>${timesGlyph}</div>`)
+
+        $('.glyphicon-remove').click(function() {
+            $(this).parent().remove();
+        })
+    })
+}
